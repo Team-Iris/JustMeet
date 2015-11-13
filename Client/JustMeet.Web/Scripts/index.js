bfa5623bafde59b9b7cdb77f8d6e82422b473a43
shunobaka <vesselin_minev@abv.bg>
2015-11-13 11:07:26 +0200

    Added project for the SPA client

---
+++ Client/JustMeet.Web/Scripts/index.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    var app = Sammy('#main-container', function () {
+        this.get('/#/', function () {
+            this.redirect('/#/home');
+        });
+
+        this.get('/#/home', function () {
+
+        });
+
+        this.get('/#/login', function () {
+        });
+
+        this.get('/#/logout', function () {
+        });
+
+        this.get('/#/register', function () {
+        });
+    });
+
+    $(function () {
+        app.run('/#/home');
+    });
+});
