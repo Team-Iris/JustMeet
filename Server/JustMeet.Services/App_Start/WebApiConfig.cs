f553eb5a18ad62c3dac2d15944469feddd74283f
ykomitov <yavork@gmail.com>
2015-11-16 01:43:41 +0200

    Fixed CORS - duplicate header issue

--- Server/JustMeet.Services/App_Start/WebApiConfig.cs
+++ Server/JustMeet.Services/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
-            config.EnableCors();
+            // config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
