5c8efc8c930b965f3a8d85097c71f1b6464bfc40
ykomitov <yavork@gmail.com>
2015-11-16 16:14:25 +0200

    Updated image table model - fixed relationship with user profile

--- Data/JustMeet.Models/Image.cs
+++ Data/JustMeet.Models/Image.cs
@@ -8,14 +8,17 @@
 
         [Required]
         [MinLength(3)]
+        [MaxLength(150)]
         public string ImageUrl { get; set; }
 
         [Required]
         [MinLength(2)]
+        [MaxLength(10)]
         public string FileExtension { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
 
+        [Required]
         public virtual User User { get; set; }
     }
 }
