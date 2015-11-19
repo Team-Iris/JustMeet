1109737438f183ec61c8dd0e6faff42c57a71df3
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-11-19 17:55:09 +0200

    GoogleDriveService now don't require browser authentication

--- Services/JustMeet.Services.Data/GoogleDriveService.cs
+++ Services/JustMeet.Services.Data/GoogleDriveService.cs
@@ -10,24 +10,29 @@
     using Google.Apis.Services;
     using JustMeet.Common.Constants;
     using JustMeet.Services.Data.Contracts;
+    using System.Collections.Specialized;
+    using System.Net;
+    using Newtonsoft.Json.Linq;
+    using Google.Apis.Auth.OAuth2.Responses;
+    using Google.Apis.Auth.OAuth2.Flows;
 
     public class GoogleDriveService : IGoogleDriveService
     {
-        private readonly UserCredential credential;
+        private const string clientId = "1082017868226-oqvt8hcalblolbf76506uvl8g2n96ar3.apps.googleusercontent.com";
+        private const string clientSecret = "NDZpYXGx8lHBWjXE1FABNwU2";
+        private const string refreshToken = "1/kubYzBrMfd33oW7WLOH6Cp-5n-G0iQ4qo3xMDCHYNKw";
+
+
         private readonly DriveService instance;
+        private UserCredential credential;
         private string[] scopes;
 
         public GoogleDriveService()
         {
             this.scopes = new string[] { DriveService.Scope.Drive };
-            using (var stream = new FileStream(@"GoogleDriveServiceFiles\\client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                this.credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                                    GoogleClientSecrets.Load(stream).Secrets,
-                                    this.scopes,
-                                    "user",
-                                    CancellationToken.None).Result;
-            }
+
+            this.credential = GetCredentialWithAccessToken(refreshToken, clientId, clientSecret, scopes);
+            this.credential.Token.Issued = DateTime.Now;
 
             this.instance = new DriveService(new BaseClientService.Initializer()
             {
@@ -38,6 +43,7 @@
 
         public IList<string> ListFilesIDs(int maxResults = GoogleDriveConstants.DefaultListFilesIDsCount)
         {
+            CheckToken(ref this.credential);
             FilesResource.ListRequest listRequest = this.instance.Files.List();
             listRequest.MaxResults = maxResults;
 
@@ -66,19 +72,21 @@
         /// <returns>Returns true on success</returns>
         public bool DeleteFileByID(string fileID)
         {
+            CheckToken(ref this.credential);
             FilesResource.DeleteRequest deleteRequest = this.instance.Files.Delete(fileID);
             var result = deleteRequest.Execute();
             return result.Length == 0;
         }
 
         /// <summary>
-        /// Uploads File into google drive.
+        /// Uploads File into Google Drive.
         /// </summary>
         /// <param name="fileBytes">byte[] of the file</param>
         /// <param name="fileNameWithExtension">File-name with extension or path ending with the filename with extension. For example: picture.jpg </param>
-        /// <returns>File ID in google drive. On error returns null.</returns>
+        /// <returns>File ID in Google Drive. On error returns null.</returns>
         public string UploadFile(byte[] fileBytes, string fileNameWithExtension)
         {
+            CheckToken(ref this.credential);
             var body = new Google.Apis.Drive.v2.Data.File();
             body.Title = fileNameWithExtension;
             body.Description = "File uploaded by JustMeet";
@@ -135,6 +143,44 @@
             }
 
             return mimeType;
+        }
+
+        private static UserCredential GetCredentialWithAccessToken(string refreshToken, string clientID, string clientSecret, string[] scopes)
+        {
+            TokenResponse token = new TokenResponse
+            {
+                RefreshToken = refreshToken
+            };
+
+            IAuthorizationCodeFlow flow = new AuthorizationCodeFlow(new AuthorizationCodeFlow.Initializer(Google.Apis.Auth.OAuth2.GoogleAuthConsts.AuthorizationUrl, Google.Apis.Auth.OAuth2.GoogleAuthConsts.TokenUrl)
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = clientID,
+                    ClientSecret = clientSecret
+                },
+                Scopes = scopes
+            });
+
+            UserCredential credential = new UserCredential(flow, "me", token);
+            try
+            {
+                bool success = credential.RefreshTokenAsync(CancellationToken.None).Result;
+            }
+            catch
+            {
+                throw;
+            }
+            return credential;
+        }
+
+        private void CheckToken(ref UserCredential credential)
+        {
+            if (credential.Token.Issued.AddSeconds(3600) <= DateTime.Now.AddSeconds(-120))
+            {
+                credential = GetCredentialWithAccessToken(refreshToken, clientId, clientSecret, this.scopes);
+                credential.Token.Issued = DateTime.Now;
+            }
         }
     }
 }
